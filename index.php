779a6c82e612f703ac95c84e940c9f0c94d4921f
ugurakcil <ugurakcil@gmail.com>
2022-04-05 18:01:49 +0300

    last stable release

--- index.php
+++ index.php
@@ -1,14 +1,59 @@
 <?php
+/**
+ * Template Name: Authors
+ *
+ * @package      Financial Data PHP
+ * @since        1.0.0
+ * @link         https://github.com/ugurakcil/financialdata-php
+ * @author       Uğur AKÇIL <ugurakcil@gmail.com>
+ * @copyright    Copyright (c) 2022, Uğur AKÇIL
+ * @license      https://github.com/ugurakcil/financialdata-php/blob/master/licence.md MIT License
+ *
+ */
+
 declare(strict_types=1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+/*
+* Configurations
+* All requests are allowed in development mode
+* Don't forget to set the development mode to false when going live
+* */
+$developmentMode = true;
+
+$allowedOrigins = array(
+    '(http(s)://)?(www\.)?website\.xyz',
+    '(http(s)://)?(www\.)?test\.my',
+    '(http(s)://)?(www\.)?my\-domain\.com'
+);
+
+/*
+* Header information, debugging, dependencies
+* */
+if($developmentMode) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    apcu_clear_cache();
+    header('Access-Control-Allow-Origin: *');
+}
+
+header('Content-type: application/json; charset=utf-8');
+
+if (!$developmentMode && isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+    foreach ($allowedOrigins as $allowedOrigin) {
+        if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+            header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+            header('Access-Control-Allow-Methods: GET'); // PUT, POST, OPTIONS, DELETE
+            break;
+        }
+    }
+}
 
 require __DIR__ . '/vendor/autoload.php';
 
-//apcu_clear_cache();
-
+/*
+* The requested assets to the class are requested in two different parts
+* */
 $financialData = new \App\FinancialData();
 
 $currencies = $financialData->getQuotes([
@@ -24,6 +69,9 @@ $currencies = $financialData->getQuotes([
     "MCL=F"     => "XTI / USD",
 ]);
 
+/*
+* A specially calculated financial instrument can be added with an outformat
+* */
 $currencies->{"KGRUSD"} = $financialData->outFormat([
     "name"      => "KGR / USD",
     "change"    => $currencies->{"GC=F"}->change,
@@ -40,46 +88,122 @@ $stocks = $financialData->getQuotes([
     "AKSEN.IS"  => "AKSEN",
     "ALARK.IS"  => "ALARK",
     "ALGYO.IS"  => "ALGYO",
-    "ALKIM.IS"  => "ALKIM"
+    "ALKIM.IS"  => "ALKIM",
+    "ARCLK.IS"  => "ARCLK",
+    "ARDYZ.IS"  => "ARDYZ",
+    "ASELS.IS"  => "ASELS",
+    "AYDEM.IS"  => "AYDEM",
+    "BAGFS.IS"  => "BAGFS",
+    "BASGZ.IS"  => "BASGZ",
+    "BERA.IS"   => "BERA",
+    "BIMAS.IS"  => "BIMAS",
+    "BIOEN.IS"  => "BIOEN",
+    "BRISA.IS"  => "BRISA",
+    "BRMEN.IS"  => "BRMEN",
+    "BRYAT.IS"  => "BRYAT",
+    "CANTE.IS"  => "CANTE",
+    "CCOLA.IS"  => "CCOLA",
+    "CIMSA.IS"  => "CIMSA",
+    "CMENT.IS"  => "CMENT",
+    "DENGE.IS"  => "DENGE",
+    "DEVA.IS"   => "DEVA",
+    "DOAS.IS"   => "DOAS",
+    "DOHOL.IS"  => "DOHOL",
+    "ECILC.IS"  => "ECILC",
+    "EGEEN.IS"  => "EGEEN",
+    "EKGYO.IS"  => "EKGYO",
+    "ENJSA.IS"  => "ENJSA",
+    "ENKAI.IS"  => "ENKAI",
+    "ERBOS.IS"  => "ERBOS",
+    "EREGL.IS"  => "EREGL",
+    "ESEN.IS"   => "ESEN",
+    "ETILR.IS"  => "ETILR",
+    "FROTO.IS"  => "FROTO",
+    "GARAN.IS"  => "GARAN",
+    "GENIL.IS"  => "GENIL",
+    "GLYHO.IS"  => "GLYHO",
+    "GOZDE.IS"  => "GOZDE",
+    "GSDHO.IS"  => "GSDHO",
+    "GUBRF.IS"  => "GUBRF",
+    "GWIND.IS"  => "GWIND",
+    "HALKB.IS"  => "HALKB",
+    "HEKTS.IS"  => "HEKTS",
+    "INDES.IS"  => "INDES",
+    "IPEKE.IS"  => "IPEKE",
+    "ISBIR.IS"  => "ISBIR",
+    "ISCTR.IS"  => "ISCTR",
+    "ISDMR.IS"  => "ISDMR",
+    "ISFIN.IS"  => "ISFIN",
+    "ISGYO.IS"  => "ISGYO",
+    "ISMEN.IS"  => "ISMEN",
+    "ISYAT.IS"  => "ISYAT",
+    "JANTS.IS"  => "JANTS",
+    "KARSN.IS"  => "KARSN",
+    "KARTN.IS"  => "KARTN",
+    "KCHOL.IS"  => "KCHOL",
+    "KENT.IS"   => "KENT",
+    "KLNMA.IS"  => "KLNMA",
+    "KONTR.IS"  => "KONTR",
+    "KORDS.IS"  => "KORDS",
+    "KOZAA.IS"  => "KOZAA",
+    "KOZAL.IS"  => "KOZAL",
+    "KRDMD.IS"  => "KRDMD",
+    "KUTPO.IS"  => "KUTPO",
+    "KZBGY.IS"  => "KZBGY",
+    "LOGO.IS"   => "LOGO",
+    "MAVI.IS"   => "MAVI",
+    "MGROS.IS"  => "MGROS",
+    "NTHOL.IS"  => "NTHOL",
+    "NUGYO.IS"  => "NUGYO",
+    "ODAS.IS"   => "ODAS",
+    "OTKAR.IS"  => "OTKAR",
+    "OTTO.IS"   => "OTTO",
+    "OYAKC.IS"  => "OYAKC",
+    "PARSN.IS"  => "PARSN",
+    "PETKM.IS"  => "PETKM",
+    "PGSUS.IS"  => "PGSUS",
+    "QNBFB.IS"  => "QNBFB",
+    "QNBFL.IS"  => "QNBFL",
+    "QUAGR.IS"  => "QUAGR",
+    "SAHOL.IS"  => "SAHOL",
+    "SASA.IS"   => "SASA",
+    "SELEC.IS"  => "SELEC",
+    "SISE.IS"   => "SISE",
+    "SKBNK.IS"  => "SKBNK",
+    "SNGYO.IS"  => "SNGYO",
+    "SNKRN.IS"  => "SNKRN",
+    "SOKM.IS"   => "SOKM",
+    "TAVHL.IS"  => "TAVHL",
+    "TBORG.IS"  => "TBORG",
+    "TCELL.IS"  => "TCELL",
+    "THYAO.IS"  => "THYAO",
+    "TKFEN.IS"  => "TKFEN",
+    "TKNSA.IS"  => "TKNSA",
+    "TKURU.IS"  => "TKURU",
+    "TOASO.IS"  => "TOASO",
+    "TRGYO.IS"  => "TRGYO",
+    "TRILC.IS"  => "TRILC",
+    "TSKB.IS"   => "TSKB",
+    "TTKOM.IS"  => "TTKOM",
+    "TTRAK.IS"  => "TTRAK",
+    "TUPRS.IS"  => "TUPRS",
+    "TURSG.IS"  => "TURSG",
+    "ULKER.IS"  => "ULKER",
+    "UTPYA.IS"  => "UTPYA",
+    "VAKBN.IS"  => "VAKBN",
+    "VESBE.IS"  => "VESBE",
+    "VESTL.IS"  => "VESTL",
+    "YKBNK.IS"  => "YKBNK",
+    "ZOREN.IS"  => "ZOREN",
+    "ZRGYO.IS"  => "ZRGYO"
 ]);
 
-?>
+/*
+* Requests from two different parties are combined on a single json
+* */
+$listAllFinancialData = (object) [
+    'currencies'    => $currencies,
+    'stocks'        => $stocks
+];
 
-<style type="text/css">
-    /* example style - will be remove */
-    .line{ 
-        display: flex;
-        margin-bottom:20px;
-    }
-    .item{
-        flex: 1;
-        border: 1px solid cadetblue;
-        padding:5px;
-    }
-    .line-first{
-        background-color:antiquewhite;
-    }
-    .line-second{
-        background-color:azure;
-    }
-</style>
-
-<div class="line line-first">
-<?php foreach($currencies as $currency): ?>
-<div class="item <?php echo $currency->direction; ?>">
-    <div><?php echo $currency->name; ?></div>
-    <div><?php echo $currency->change; ?>%</div>
-    <div><?php echo $currency->price; ?></div>
-</div>
-<?php endforeach; ?>
-</div>
-
-<div class="line line-second">
-<?php foreach($stocks as $stock): ?>
-<div class="item <?php echo $stock->direction; ?>">
-    <div><?php echo $stock->name; ?></div>
-    <div><?php echo $stock->change; ?>%</div>
-    <div><?php echo $stock->price; ?></div>
-</div>
-<?php endforeach; ?>
-</div>
+echo json_encode($listAllFinancialData);
--- src/FinancialData.php
+++ src/FinancialData.php
@@ -1,5 +1,18 @@
 <?php
+/**
+ * Template Name: Authors
+ *
+ * @package      Financial Data PHP
+ * @since        1.0.0
+ * @link         https://github.com/ugurakcil/financialdata-php
+ * @author       Uğur AKÇIL <ugurakcil@gmail.com>
+ * @copyright    Copyright (c) 2022, Uğur AKÇIL
+ * @license      https://github.com/ugurakcil/financialdata-php/blob/master/licence.md MIT License
+ *
+ */
+
 declare(strict_types=1);
+
 namespace App;
 
 use Scheb\YahooFinanceApi\ApiClient;
@@ -14,10 +27,17 @@ class FinancialData
         $this->client = ApiClientFactory::createApiClient();
     }
 
+    /*
+    * Generates a special id for caching using the array's information
+    * */
     private function generateListId(array $list): string {
         return substr(base64_encode(implode('',$list)),0,20);
     }
 
+    /*
+    * It returns the instruments requested with Array 
+    * by checking whether they are in the cache or not
+    * */
     public function getQuotes(array $list): object
     {
         $apcuId = $this->generateListId($list);
@@ -30,6 +50,9 @@ class FinancialData
         }
     }
 
+    /*
+    * Makes and arranges the desired instruments into an object
+    * */
     private function setQuotes(array $list): object
     {
         $quotes = $this->client->getQuotes(array_keys($list));
@@ -53,6 +76,10 @@ class FinancialData
         return (object) $output;
     }
 
+    /*
+    * Used to apply filters such as type conversion 
+    * and number format of the information of financial instruments
+    * */
     public function outFormat(array $pureData) 
     {
         return (object) [
